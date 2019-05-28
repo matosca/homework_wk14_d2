122292b5032331a462796d996a4d45074e85b8ba
marisol <mtoscanor37@gmail.com>
2019-05-28 19:55:03 +0100

    Added style to boxes

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,10 +4,10 @@ class Song extends Component {
   render () {
     return (
       <div className="song">
+        <img src={this.props.children} alt=""></img>
         <h5><a href={this.props.audio}>{this.props.title}</a></h5>
         <h5>{this.props.artist}</h5>
         <h5>{this.props.category}</h5>
-        <img src={this.props.children} alt=""></img>
       </div>
 
     );
